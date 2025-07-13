c614df6dea71492b7f8ecc5b66bdd997da94b0a7
Ujjwal Jayswal <ujjwaljayswal9@gmail.com>
2025-07-13 15:22:45 +0530

    Debug, set to false

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'your-default-insecure-key')
 
 
-DEBUG = True
+DEBUG = False
 
 
 ALLOWED_HOSTS = ['*']
